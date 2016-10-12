8c592c2a60c164971164004de8b30073ae7b9c26
vivekpaypal <vivek.125@gmail.com>
2016-10-13 00:12:29 +0530

    imports

--- SeleniumTestProject/src/main/java/com/example/freelance/test/DriverFactory.java
+++ SeleniumTestProject/src/main/java/com/example/freelance/test/DriverFactory.java
@@ -1,6 +1,9 @@
 package com.example.freelance.test;
 
-import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class DriverFactory {
 	
